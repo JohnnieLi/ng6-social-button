ebc7ab87754810d940d11af8e17a1a9465212047
Jiangqi <jiangqi2015@outlook.com>
2018-08-13 10:33:07 -0400

    init

---
+++ src/public_api.ts
@@ -0,0 +1,12 @@
+/*
+ * Public API Surface of ng6-social-button
+ */
+
+export * from './lib/social.service';
+export * from './lib/providers/facebookProvider';
+export * from './lib/providers/googleProvider';
+export * from './lib/buttons/facebookLoginButton';
+export * from './lib/buttons/googleLoginButton';
+export * from './lib/buttons/facebookSharingButton';
+
+export * from './lib/ng6-social-button.module';
